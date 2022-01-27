0732008dda2ebc0957a63e874ea0be96b6f75d9d
Adriano Moreira <adr_0603@yahoo.com.br>
2022-01-26 23:38:05 -0300

    page Chat created

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,9 @@
 import { Box, Button, Text, TextField, Image } from "@skynexui/components";
+import { useState } from "react";
+import { useRouter } from 'next/router'
 import appConfig from "../config.json";
+import { useFormik } from 'formik';
 
-function GlobalStyle() {
-  return (
-    <>
-      <style global jsx>
-        {`
-          * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            list-style: none;
-          }
-          body {
-            font-family: "Open Sans", sans-serif;
-          }
-          /* App fit Height */
-          html,
-          body,
-          #__next {
-            min-height: 100vh;
-            display: flex;
-            flex: 1;
-          }
-          #__next {
-            flex: 1;
-          }
-          #__next > * {
-            flex: 1;
-          }
-          /* ./App fit Height */
-        `}
-      </style>
-    </>
-  );
-}
 
 function Titulo(props) {
   const Tag = props.tag || "h1";
@@ -52,26 +21,18 @@ function Titulo(props) {
   );
 }
 
-// export default function HomePage() {
-//   return (
-//     <div>
-//         <GlobalStyle />
-//         <Titulo tag="h1">Boas vindas de volta!</Titulo>
-//         <h2>Discord - Alura Matrix</h2>
-//     </div>)
-
 export default function PaginaInicial() {
-  const username = "adrianomoreira86";
+  const [username, setUsername] = useState("");
+  const router = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          backgroundColor: appConfig.theme.colors.primary[500],
+          backgroundColor: appConfig.theme.colors.primary[200],
           backgroundImage:
             "url(https://virtualbackgrounds.site/wp-content/uploads/2020/09/organized-server-room-1536x864.jpeg)",
           backgroundRepeat: "no-repeat",
@@ -100,6 +61,11 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit= {(event) => {
+              event.preventDefault();
+              router.push('/chat');
+              // window.location.href = '/chat';
+            } }
             styleSheet={{
               display: "flex",
               flexDirection: "column",
@@ -122,6 +88,11 @@ export default function PaginaInicial() {
             </Text>
 
             <TextField
+              value={username}
+              onChange={(event) => {
+                const { value } = event.target;
+                setUsername(value);
+              }}
               fullWidth
               textFieldColors={{
                 neutral: {
